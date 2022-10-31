ef07ef0f4fe8a0d94d10c752f5a5abe7d9b582a6
Kesha Mae Panaligan <21-04569@g.batstate-u.edu.ph>
2022-10-31 22:16:39 +0800

    Update netSpeed.py

--- netSpeed.py
+++ netSpeed.py
@@ -4,8 +4,8 @@ import speedtest
 def speedcheck():
     sp = speedtest.Speedtest()
     sp.get_servers()
-    down = str(round(sp.download()/(10**6),3))+"Mbps"
-    up = str(round(sp.upload()/(10**6),3))+"Mbps"
+    down = str(round(sp.download()/(10**6),2))+"Mbps"
+    up = str(round(sp.upload()/(10**6),2))+"Mbps"
     lab_down.config(text=down)
     lab_up.config(text=up)
 
